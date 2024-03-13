c0eae7d09b1a36fc830db0e390667a1df035d7f6
viwwzx <144542462+viwwzx@users.noreply.github.com>
2024-03-13 14:14:24 +0300

    Add files via upload

--- pz_9.py
+++ pz_9.py
@@ -1,10 +1,12 @@
 # Дан словарь с четным количеством элементов.
 # Найти суммы значений элементов первой и второй половин
 # с использованием функции. Результаты вывести на экран
-even_dic = {1: 3, 2: 5, 3: 1, 4: 9}
+even_dict = {1: 3, 2: 5, 3: 1, 4: 9}
 
-first_half = sum(list(even_dic.values())[:len(even_dic)//2])
-second_half = sum(list(even_dic.values())[len(even_dic)//2:])
+def summa(x):
+    first_half = sum(list(x.values())[:len(x)//2])
+    second_half = sum(list(x.values())[len(x)//2:])
+    return first_half, second_half
 
-print(first_half)
-print(second_half)
+end1, end2 = summa(even_dict)
+print(f'Сумма первой половины: {end1}\nСумма второй половины: {end2}')
